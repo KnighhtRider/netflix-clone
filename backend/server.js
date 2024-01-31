8c7f07906b9ab2e0cb4587090c8f6312549114ed
Vivek Thakur <110936458+KnighhtRider@users.noreply.github.com>
2024-02-01 03:35:42 +0530

    :car: Backend Setup

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://vivekthakur6259:vivek123456@netflix.vk0lp2y.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.listen(5000, () => {
+  console.log("server started at 5000");
+});
